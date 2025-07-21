0e7de5c3a3ebf8cd2fae96744941a6f5034d7204
Maria Eduarda Moreti <mmoreti@edu.unisinos.br>
2025-07-21 15:41:54 -0300

    Aula5 - Pratica

---
+++ aula5-Pratica/exercicioString.js
@@ -0,0 +1,12 @@
+                //0123456789
+let umaString = "Javascript é incrível"
+umaString[5]
+umaString.charAt(5)
+
+//console.log(umaString[5])
+//console.log(umaString.charAt(5))
+//console.log(umaString.replace("incrível", "poderoso"))
+console.log(umaString.length)
+console.log(umaString.slice(10,21))
+console.log(umaString.concat(" Vamos aprender!")) //adiciona no final
+
